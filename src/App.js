d9291e4b3aa1d607cd17940485b735c313d823ee
bellamard <belamard@gmail.com>
2022-12-04 12:59:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Login from "./pages/login";
+import NotFound from "./pages/notFound";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <main>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} exact />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </main>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/pages/login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, Form, FormGroup, Input, Label } from reactstrap;
+import logo from '../logo.svg';
+import Header from '../component/header';
+
+function login(props) {
+    return (
+        <div>
+            <Header />
+
+        </div>
+    );
+}
+
+export default login;
